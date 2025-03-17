59788ea226dfb7b13d8f326f1fc7b919f66fd122
“Harsh6575” <harsh.190410107146@gmail.com>
2025-03-17 14:41:56 +0530

    feat: adding metadata for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,10 +12,34 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const APP_URL = `https://harsh-vansjaliya.vercel.app`;
+
 export const metadata: Metadata = {
-  title: "Harsh Vansjaliya",
-  description: "Harsh Vansjaliya's portfolio page.",
+  title: "Harsh Vansjaliya | Software Developer",
+  description: "Explore Harsh Vansjaliya's portfolio, featuring projects, skills, and experience in web development.",
+  openGraph: {
+    title: "Harsh Vansjaliya | Software Developer",
+    description: "Check out my portfolio showcasing my work in Next.js, React, and more.",
+    url: `${APP_URL}/`,
+    type: "website",
+    images: [
+      {
+        url: `${APP_URL}/preview.png`, // image URL
+        width: 1200,
+        height: 630,
+        alt: "Harsh Vansjaliya Portfolio Preview",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Harsh Vansjaliya | Software Developer",
+    description: "Explore my portfolio showcasing projects and skills.",
+    images: [`${APP_URL}/preview.png`], // Same as Open Graph
+  },
+  keywords: ["Harsh Vansjaliya", "Portfolio", "Developer", "Harsh", "Vansjaliya", "Harsh Vansjaliya's portfolio"],
 };
+
 
 export default function RootLayout({
   children,
